3d02e4dae818eb63e4e209e7b2b192309d56fd2a
xovest <79008917+xovest@users.noreply.github.com>
2022-04-16 17:44:02 +0300

    finally got them

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
   return (
     <>
       <header>
         <h1>Them crud table</h1>
       </header>
-      <Form />
+      <Form inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} setStatus={setStatus} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,26 @@
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const completeHandler = () => {
+    setTodos(todos.map(el => {
+      if (el.id === todo.id) {
+        return {
+          ...el, completed: !el.completed
+        }
+      }
+      return el
+    }))
+  }
+
+  const deleteHandler = () => {
+    setTodos(todos.filter(el => el.id !== todo.id))
+  }
+
+  return (
+    <div>
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn"><i className="fas fa-check"></i></button>
+      <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
+    </div>
+  )
+}
+
+export default Todo
